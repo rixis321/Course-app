3194b32520a2b4cb123d217c0e984d1decd9bd34
Konrad Sygut <konrsyg825@student.polsl.pl>
2023-05-29 01:35:35 +0200

    sign-in page in progress

---
+++ lib/pages/sign_in/bloc/signin_blocs.dart
@@ -0,0 +1,18 @@
+import 'package:course_app/pages/sign_in/bloc/signin_events.dart';
+import 'package:course_app/pages/sign_in/bloc/signin_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInBloc extends Bloc<SignInEvent,SignInState>{
+  SignInBloc():super(const SignInState()){
+    on<EmailEvent>(_emailEvent);
+
+    on<PasswordEvent>(_passwordEvent);
+  }
+
+  void _emailEvent(EmailEvent event, Emitter<SignInState> emit){
+    emit(state.copyWith(email: event.email));
+  }
+  void _passwordEvent(PasswordEvent event, Emitter<SignInState> emit){
+    emit(state.copyWith(email: event.password));
+  }
+}
---
+++ lib/pages/sign_in/signin_controller.dart
@@ -0,0 +1,43 @@
+import 'package:course_app/pages/sign_in/bloc/signin_blocs.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInController {
+  final BuildContext context;
+  const SignInController({required this.context});
+
+  Future<void> handleSignIn(String type) async {
+    try {
+      if (type == "email") {
+        final state = context.read<SignInBloc>().state;
+        String emailAddress = state.email;
+        String password = state.password;
+        if (emailAddress.isEmpty) {
+          //
+        }
+        if (password.isEmpty) {
+          //
+        }
+        try {
+          final credential = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(
+                  email: emailAddress, password: password);
+          if(credential.user== null){
+            //
+          }
+          if(!credential.user!.emailVerified){
+            //
+          }
+
+          var user = credential.user;
+          if(user!=null){
+
+          }else{
+
+          }
+        } catch (e) {}
+      }
+    } catch (e) {}
+  }
+}
