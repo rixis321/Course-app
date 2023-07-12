10b026817c18b81de0b0f771d7a7b2a5a1695711
Brystol <odido160@gmail.com>
2023-07-12 21:05:39 +0200

    Gathering data of number of lessons in each course from backend to frontend

--- lib/pages/course/course_detail/course_detail_controller.dart
+++ lib/pages/course/course_detail/course_detail_controller.dart
@@ -1,5 +1,6 @@
 import 'package:course_app/common/apis/course_api.dart';
 import 'package:course_app/common/entities/course.dart';
+import 'package:course_app/common/entities/entities.dart';
 import 'package:course_app/common/routes/names.dart';
 import 'package:course_app/common/widgets/flutter_toast.dart';
 import 'package:course_app/pages/course/course_detail/bloc/course_detail_blocs.dart';
@@ -9,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
+import '../../../common/apis/lesson_api.dart';
+
 class CourseDetailController{
   final BuildContext context;
 
@@ -16,10 +19,11 @@ class CourseDetailController{
 
   void init() async{
     final args = ModalRoute.of(context)!.settings.arguments as Map;
-    asyncLoadAllData(args["id"]);
+    asyncLoadCourseData(args["id"]);
+    asyncLoadLessonData(args["id"]);
   }
 
-  asyncLoadAllData(int? id) async{
+  asyncLoadCourseData(int? id) async{
     CourseRequestEntity courseRequestEntity = CourseRequestEntity();
     courseRequestEntity.id = id;
 
@@ -36,7 +40,13 @@ class CourseDetailController{
       toastInfo(msg: "Something went wrong");
       print("-----------------------Error code ${result.code}-----------------------");
     }
+  }
 
+  asyncLoadLessonData(int? id) async {
+    LessonRequestEntity lessonRequestEntity = LessonRequestEntity();
+    lessonRequestEntity.id = id;
+    var result = await LessonAPI.lessonList(params:lessonRequestEntity);
+    print('---------my lesson data ${result.data?.length.toString()}---------');
   }
 
   Future<void> goBuy(int? id) async {
